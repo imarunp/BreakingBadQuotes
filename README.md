0dbc9e6788c058ebb2064410901b486d63de206e
Arun <arcanearun@gmail.com>
2023-09-25 11:20:11 +0545

    App init

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Quote app which will display a dialog from Breaking Bad series. API used : https://breakingbadquotes.xyz/
---
+++ src/Quotes.jsx
@@ -0,0 +1,125 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+function Quotes() {
+  const [quote, setQuote] = useState({});
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    getData();
+  }, [count]);
+  function getData() {
+    axios
+      .get('https://api.breakingbadquotes.xyz/v1/quotes')
+      .then((res) => setQuote(res.data[0]));
+  }
+
+  return (
+    <div className='container'>
+      <Card
+        sx={{
+          p: 3,
+          width: '75%',
+          height: 500,
+          maxWidth: 400,
+          backgroundColor: '#edede9',
+        }}
+      >
+        <CardContent>
+          <Typography
+            color='#3a5a40'
+            fontWeight={800}
+            fontFamily='Poppins'
+            gutterBottom
+            variant='h4'
+            component='div'
+            textAlign='center'
+          >
+            <span className='title'>Br</span>eaking
+          </Typography>
+          <Typography
+            color='#3a5a40'
+            fontWeight={800}
+            fontFamily='Poppins'
+            gutterBottom
+            variant='h4'
+            component='div'
+            textAlign='center'
+            sx={{ ml: -1 }}
+          >
+            <span className='title'>Ba</span>d
+          </Typography>
+          <Box
+            sx={{
+              alignItems: 'center',
+              textAlign: 'center',
+              border: '4px solid #3a5a40',
+              borderRadius: '2%',
+            }}
+            width='75%'
+            height={150}
+            mt={4}
+            pt={5}
+            pl={3}
+            pb={3}
+            pr={3}
+          >
+            <Typography sx={{ fontWeight: 'bold', pb: 2 }} variant='body1'>
+              "{quote.quote}"
+            </Typography>
+            <Typography variant='body1'>- {quote.author}</Typography>
+          </Box>
+        </CardContent>
+        <CardActions>
+          <Button
+            onClick={() => setCount((count) => count + 1)}
+            sx={{
+              ml: 'auto',
+              mr: 'auto',
+              mt: 2,
+              border: '2px solid #3a5a40 ',
+              backgroundColor: '#3a5a40',
+              textAlign: 'center',
+              color: '#fff',
+              fontWeight: 600,
+              ':hover': { fontWeight: 800, color: '#344e41' },
+            }}
+            size='large'
+          >
+            Get a random Quote
+          </Button>
+        </CardActions>
+        <Typography
+          sx={{
+            mt: 2,
+            display: 'flex',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+          }}
+          variant='body2'
+          component='span'
+        >
+          © Arun Pandey
+          <IconButton
+            sx={{ color: '#3a5a40' }}
+            size='small'
+            href='https://github.com/imarunp/Breaking-bad-quotes'
+          >
+            <GitHubIcon />
+          </IconButton>
+        </Typography>
+      </Card>
+    </div>
+  );
+}
+export default Quotes;
